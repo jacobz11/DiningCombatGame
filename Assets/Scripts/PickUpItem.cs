50d26febfb5ec221b7c74141cd1a5f8c2c43c13b
evduanG <eviatar.duany@gmail.com>
2023-01-10 10:38:18 +0200

    Trying to understand the code and arrange PickUpItem

--- Assets/Scripts/PickUpItem.cs
+++ Assets/Scripts/PickUpItem.cs
@@ -1,24 +1,33 @@
 using UnityEngine;
 using DiningCombat;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
+using System;
 
 public class PickUpItem : MonoBehaviour
 {
-    private const float k_MaxDistanceToPickUp = 2f;
-    private Transform m_PickUpPoint;
-    private Transform m_Player;
-    private Rigidbody m_Rb;
-    private bool m_IsPowerPressed;
-    private bool IsPowerPressed
+    // Hand-State enum
+    private const byte k_HandStateFree = 0;
+    private const byte k_HandStateHoldsObj = 1;
+    private const byte k_HandStatePowering = 2;
+    private const byte k_HandStateThrowing = 3;
+    private int m_HandState;
+    private int HandState
     {
         get 
         { 
-            return m_IsPowerPressed; 
+            return m_HandState;
         }
         set
         {
-            m_IsPowerPressed = value;
+            m_HandState = value % 4;
         }
     }
+    private const float k_MaxDistanceToPickUp = 2f;
+
+    private Transform m_PickUpPoint;
+    private Transform m_Player;
+    private Rigidbody m_Rb;
+    
 
     [SerializeField]
     public float m_PickUpDistance;
@@ -27,12 +36,12 @@ public class PickUpItem : MonoBehaviour
     private bool m_IsReadyToThrow;
     public bool IsReadyToThrow
     {
-        get 
-        { 
+        get
+        {
             return m_IsReadyToThrow;
         }
-        set 
-        { 
+        set
+        {
             m_IsReadyToThrow = value;
         }
     }
@@ -41,9 +50,9 @@ public class PickUpItem : MonoBehaviour
     private bool m_HasItemPicked;
     public bool HasItemPicked
     {
-        get 
-        { 
-            return m_HasItemPicked; 
+        get
+        {
+            return m_HasItemPicked;
         }
         set
         {
@@ -66,11 +75,13 @@ public class PickUpItem : MonoBehaviour
         }
     }
 
-    private bool isAddForce()
+    public bool IsPowerPress
     {
-        return IsPowerPressed && HasItemPicked && IsReadyToThrow;
+        get
+        {
+            return Input.GetKeyUp(GameGlobal.k_PowerKey);
+        }
     }
-
     protected void Start()
     {
         m_Rb = GetComponent<Rigidbody>();
@@ -80,50 +91,135 @@ public class PickUpItem : MonoBehaviour
 
     protected void Update()
     {
-        // var Update 
-        IsPowerPressed = Input.GetKeyUp(GameGlobal.k_PowerKey);
-        m_PickUpDistance = Vector3.Distance(m_Player.position, transform.position);
-        
-        if (isAddForce())
+
+        if (Input.GetKey(GameGlobal.k_PowerKey) && HasItemPicked == true && IsReadyToThrow)
         {
             ForceMulti += 1400 * Time.deltaTime;
         }
 
-        bool isDistanceValid = m_PickUpDistance <= k_MaxDistanceToPickUp;
-        bool doesntHaveItme = m_PickUpPoint.childCount < 1;
+        m_PickUpDistance = Vector3.Distance(m_Player.position, transform.position);
 
-        if (IsPowerPressed && ! HasItemPicked && isDistanceValid && doesntHaveItme)
+        if (m_PickUpDistance <= 2)
         {
-            GetComponent<Rigidbody>().useGravity = false;
-            GetComponent<BoxCollider>().enabled = false;
-            this.transform.position = m_PickUpPoint.position;
-            this.transform.parent = GameObject.Find(GameGlobal.k_GameObjectPickUpPoint).transform;
+            if (Input.GetKeyDown(GameGlobal.k_PowerKey) && HasItemPicked == false && m_PickUpPoint.childCount < 1)
+            {
+                GetComponent<Rigidbody>().useGravity = false;
+                GetComponent<BoxCollider>().enabled = false;
+                this.transform.position = m_PickUpPoint.position;
+                this.transform.parent = GameObject.Find(GameGlobal.k_GameObjectPickUpPoint).transform;
 
-            HasItemPicked = true;
-            ForceMulti = 0;
+                HasItemPicked = true;
+                ForceMulti = 0;
+            }
         }
 
-        if (IsPowerPressed && HasItemPicked)
+        if (Input.GetKeyUp(GameGlobal.k_PowerKey) && HasItemPicked == true)
         {
             IsReadyToThrow = true;
 
             if (ForceMulti > 10)
             {
-                //  throw away
                 m_Rb.AddForce(m_Player.transform.forward * ForceMulti);
-
-                // reset all var 
+                this.transform.parent = null;
                 GetComponent<Rigidbody>().useGravity = true;
                 GetComponent<BoxCollider>().enabled = true;
-                this.transform.parent = null;
                 HasItemPicked = false;
-                IsReadyToThrow = false;
-                ForceMulti = 0;
-            }
 
+                ForceMulti = 0;
+                IsReadyToThrow = false;
+            }
             ForceMulti = 0;
         }
+
+
+
+        switch (HandState)
+        {
+            case k_HandStateFree:
+                handFree();
+                break;
+            case k_HandStateHoldsObj:
+                handHoldsObj();
+                break;
+            case k_HandStatePowering:
+                handPowering();
+                break;
+            case k_HandStateThrowing:
+                handThrowing();
+                break;
+            default:
+                HandState = k_HandStateFree;
+                break;
+        }
     }
+    private void handFree()
+    {
+        // TODO:
+        // This will be a function that checks if an object can be picked up
+        if (haveAvailableGameObject())
+        {
+            // pickUpItem();
+            HandState++;
+        }
+    }
+
+    private bool haveAvailableGameObject()
+    {
+       // TODO: 
+       return false;
+    }
+
+    private void handHoldsObj()
+    {
+        // TODO:
+        // This function does when there is an object
+        if (IsPowerPress)
+        {
+            HandState++;
+        }
+    }
+    private void handPowering()
+    {
+        // TODO:
+        // This function does when you press the power button
+        if (IsPowerPress)
+        {
+            ForceMulti += 1400 * Time.deltaTime;
+        }
+        else
+        {
+            throwAway();
+            HandState++;
+        }
+
+    }
+
+    private void throwAway()
+    {
+        m_Rb.AddForce(m_Player.transform.forward * ForceMulti);
+    }
+
+    private void handThrowing()
+    {
+        // TODO:
+        // This function calls after a throw
+
+        // remove all the ...  
+        this.transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<BoxCollider>().enabled = true;
+        HasItemPicked = false;
+
+        ForceMulti = 0;
+        IsReadyToThrow = false;
+
+        // and do handFree
+        handFree();
+    }
+
+
+
+
     protected void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.CompareTag(GameGlobal.k_TagCapsule))
@@ -143,6 +239,148 @@ public class PickUpItem : MonoBehaviour
     }
 }
 
+
+
+/*using UnityEngine;
+using DiningCombat;
+
+public class PickUpItem : MonoBehaviour
+{
+    private Transform m_PickUpPoint;
+    private Transform m_Player;
+    private Rigidbody m_Rb;
+
+    [SerializeField]
+    private float m_PickUpDistance;
+
+    [SerializeField]
+    private float m_ForceMulti;
+
+    public float ForceMulti
+    {
+        get { return m_ForceMulti; }
+        set { m_ForceMulti = value; }
+    }
+
+    [SerializeField]
+    private bool m_ReadyToThrow;
+
+    [SerializeField]
+    private bool m_ItemIsPicked;
+    private const float k_MaxForcex = 1900;
+    private const bool k_Enabled = true;
+    private const bool k_UseGravity = true;
+
+    private bool IsCanThrowObj
+    {
+        get
+        {
+            return Input.GetKey(GameGlobal.k_PowerKey) && m_ItemIsPicked;
+        }
+    }
+
+    private Transform findPickUpPoint()
+    {
+        return GameObject.Find(GameGlobal.k_GameObjectPickUpPoint).transform;
+    }
+
+    protected void Start()
+    {
+        m_Rb = GetComponent<Rigidbody>();
+        m_Player = GameObject.Find(GameGlobal.k_GameObjectPlayer).transform;
+        m_PickUpPoint = findPickUpPoint();
+    }
+
+    // Update is called once per frame
+    protected void Update()
+    {
+        bool isAddForce = IsCanThrowObj && m_ReadyToThrow && m_ForceMulti < k_MaxForcex;
+
+        if (isAddForce)
+        {
+            m_ForceMulti += 500 * Time.deltaTime;
+            PowerCounter.PowerValue = m_ForceMulti;
+        }
+
+        m_PickUpDistance = Vector3.Distance(m_Player.position, transform.position);
+
+        if (m_PickUpDistance <= 2 && IsThrowGameObj)
+        {
+            EnabledGravity = !k_UseGravity;
+            EnabledBoxCollider = !k_Enabled;
+            this.transform.position = m_PickUpPoint.position;
+            this.transform.parent = findPickUpPoint();
+
+            m_ItemIsPicked = true;
+            m_ForceMulti = 0;
+        }
+
+        if (IsCanThrowObj)
+        {
+            m_ReadyToThrow = true;
+
+            if (m_ForceMulti > 10)
+            {
+                m_Rb.AddForce(m_Player.transform.forward * m_ForceMulti);
+                this.transform.parent = null;
+
+                EnabledGravity = k_UseGravity;
+                EnabledBoxCollider = k_Enabled;
+
+                m_ItemIsPicked = false;
+                m_ReadyToThrow = false;
+                m_ForceMulti = 0;
+            }
+
+            m_ForceMulti = 0;
+        }
+    }
+    private bool EnabledGravity
+    {
+        get
+        {
+            return m_Rb.useGravity;
+        }
+        set
+        {
+            m_Rb.useGravity =value;
+        }
+    }
+
+    private bool EnabledBoxCollider
+    {
+        get
+        {
+            return GetComponent<BoxCollider>().enabled;
+        }
+        set
+        {
+            EnabledBoxCollider = value;
+        }
+    }
+
+    private bool IsThrowGameObj
+    {
+        get
+        {
+            bool isPrssTo = Input.GetKeyDown(GameGlobal.k_PowerKey);
+            bool haveItemTo = m_PickUpPoint.childCount < 1;
+
+            return isPrssTo && !m_ItemIsPicked && haveItemTo;
+        }
+    }
+
+    protected void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag(GameGlobal.k_TagCapsule))
+        {
+            ScoreCounter.ScoreValue++;
+            Destroy(col.gameObject, 1);
+            Destroy(gameObject, 1);
+        }
+    }
+}
+*/
 
 
 /*using UnityEngine;
