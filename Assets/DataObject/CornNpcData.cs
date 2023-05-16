fb13f1932c8c57b3a7304040f19833fd0ca1fdd0
evduanG <eviatar.duany@gmail.com>
2023-05-16 15:11:14 +0300

    removing CornNpcData

--- Assets/DataObject/CornNpcData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using UnityEngine;
-[Serializable]
-internal class CornNpcData
-{
-    public GameObject m_Popcorn;
-    public int m_NumOfExpltin;
-    public float m_PopcornPower;
-}
