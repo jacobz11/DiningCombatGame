114d4d654b7c40897b1a30522aeef82880d2933d
evduanG <eviatar.duany@gmail.com>
2023-05-29 21:03:26 +0300

    add PlayerChannel

---
+++ Assets/Scripts/Manger/PlayerChannel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Manger
+{
+    public class PlayerChannel
+    {
+        public struct PlayerStatus
+        {
+            public int Id;
+            public string Name;
+            public float Score;
+            public int Kills;
+            public bool IsAlive;
+        }
+        private Dictionary<string, PlayerStatus> m_Table = new Dictionary<string, PlayerStatus>();
+        
+        public void UpdatePlayer(string i_Player, float i_Score, int i_Kills)
+        {
+            if (!m_Table.ContainsKey(i_Player))
+            {
+                UnityEngine.Debug.Log($"Player-Channel is not Contains Key {i_Player}");
+                return;
+            }
+
+            PlayerStatus playerStatus = m_Table[i_Player];
+            m_Table[i_Player]= new PlayerStatus()
+            {
+                Id = playerStatus.Id, 
+                Name = playerStatus.Name, 
+                Score = i_Score,
+                Kills = i_Kills,
+                IsAlive = playerStatus.IsAlive,
+            };
+        }
+
+        public List<PlayerStatus> GetTable()
+        {
+            return m_Table.Values.ToList().OrderBy(p => p.IsAlive).ThenBy(p => p.Score).ToList();
+        }
+    }
+}
