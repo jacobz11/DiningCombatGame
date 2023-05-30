b223f4fb225b67338752a58ed5a7ca356631423d
evduanG <eviatar.duany@gmail.com>
2023-05-30 17:22:48 +0300

    Lowering maximum MaxPower

--- Assets/Scripts/Util/ScriptableObjects/PoweringDataSo.cs
+++ Assets/Scripts/Util/ScriptableObjects/PoweringDataSo.cs
@@ -10,19 +10,19 @@ namespace DiningCombat.DataObject
         [Range(100f, 5000f)]
         [Tooltip("the max Power  thet the player can charge")]
         public float m_MaxPower = 3000;
+        [Range(10f, 1000f)]
+        [Tooltip("The minimum force to make a throw")]
+        public float m_MinPower = 50.0f;
         [Range(10f, 2000f)]
         [Tooltip("The increment in each update, it will be multiplied by delta time")]
         public float m_DataTimeAddingPower = 1400;
         [Range(10f, 2000f)]
         [Tooltip("The maximum time the AI can charge")]
         public float m_MaxPoweringTime = 10.0f;
-        [Range(10f, 1000f)]
-        [Tooltip("The minimum force to make a throw")]
-        public float m_MinPower = 50.0f;
-        [Range(10f, 1000f)]
+        [Range(0f, 10f)]
         [Tooltip("AI : The Update Rate Find Closesr Player")]
         public float m_UpdateRate = 1.5f;
-        [Range(10f, 1000f)]
+        [Range(0f, 100f)]
         [Tooltip("AI : Min Distance To Target")]
         public float m_MinDistanceToTarget = 7f;
         public float NormalizingPower(float power)
